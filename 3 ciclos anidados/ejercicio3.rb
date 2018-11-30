8ec1a79b6338570ebea18cd0084ad63c700e4999
Vanessa Frenkel <vanefrenkel@gmail.com>
2018-11-30 00:59:50 -0300

    ultimo ejerecicio

--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,14 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+puts "Ingresa un número"
+n = gets.chomp.to_i
+i = 1
+while n != 0 
+    puts "#{n} x #{i} = #{ n * i}"
+    i = i + 1
+    puts "para continuar, ingresa un número distinto de 0"
+    a = gets.chomp.to_i
+    n = 0 if a == 0
+end
